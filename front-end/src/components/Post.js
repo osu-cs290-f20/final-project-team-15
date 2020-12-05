6f29c431f4ffbdf34e5a42c2fed0318ee515fb31
ninjaco1 <nguyean6@users.noreply.github.com>
2020-12-05 00:25:24 -0800

    Navbar

---
+++ front-end/src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Post(props){
+    return(
+        <div>
+            {/* first name, last name, age, blood type*/}
+        </div>
+    )
+}
+
+export default Post;
